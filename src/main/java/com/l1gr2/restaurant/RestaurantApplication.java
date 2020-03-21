5c90a4e7732e5f42ba276abffcab7a864f65bea8
Jakub <jakubkozimor399@gmail.com>
2020-03-21 15:45:49 +0100

    Initial commit

---
+++ src/main/java/com/l1gr2/restaurant/RestaurantApplication.java
@@ -0,0 +1,44 @@
+package com.l1gr2.restaurant;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class RestaurantApplication extends Application {
+    private ConfigurableApplicationContext springContext;
+    private Parent rootNode;
+    private FXMLLoader fxmlLoader;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(RestaurantApplication.class);
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(springContext::getBean);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        fxmlLoader.setLocation(getClass().getResource("/fxml/first-page.fxml"));
+        rootNode = fxmlLoader.load();
+
+        primaryStage.setTitle("Hello World");
+        Scene scene = new Scene(rootNode, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        springContext.close();
+    }
+}
---
+++ src/test/java/com/l1gr2/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1gr2.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
