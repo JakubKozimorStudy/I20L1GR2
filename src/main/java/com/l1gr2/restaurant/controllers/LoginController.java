f4ee05d0e562a7bcb124070b8ad804d60bff8690
Lukasz <lukasz.kusta@interia.pl>
2020-04-02 16:39:31 +0200

    Add login panel .fxml and LoginController

---
+++ src/main/java/com/l1gr2/restaurant/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.l1gr2.restaurant.controllers;
+
+import javafx.fxml.FXML;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class LoginController {
+
+
+    @FXML
+    void initialize() {
+    }
+}
